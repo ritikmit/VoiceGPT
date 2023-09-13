fb36dc1789880073f4928d65d3365b9c94e3d3b7
Aneeket Mangal <amangal@microsoft.com>
2023-09-13 18:49:32 +0530

    changes

--- services/gpt/index.js
+++ services/gpt/index.js
@@ -1,6 +1,6 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
-const endpoint = 0;
-const azureApiKey = 0;
+const endpoint = 'https://ausopenai.azure-api.net'
+const azureApiKey = 'd02f73a957c948abb66d0baf33e1b214'
 
 const messages = [
   { role: "system", content: "For further converstaion, keep your responses concise and brief" },
@@ -8,10 +8,11 @@ const messages = [
 ];
 
 async function getGptResponse(conversation) {
+  let modifiedConversation = [{content: "Act like you are an daily assistant, keep your answers short and concise. Prefer one or two lines sentences, whereever possible", "role": "user"}, ...conversation]
   const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
   const deploymentId = "gpt-35-turbo-16k";
   let message = "Could not generate response. Try again!";
-  const result = await client.getChatCompletions(deploymentId, conversation);
+  const result = await client.getChatCompletions(deploymentId, modifiedConversation);
 
   console.log(result.choices);
   if(result.choices[0]?.message !== undefined){
